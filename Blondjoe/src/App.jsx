3871a42f8e83cb01a6c1bc9d41730cf3c332c189
Tinatajik <tinatajikrostamii@gmail.com>
2024-04-07 02:35:20 +0330

    navbar

--- Blondjoe/src/App.jsx
+++ Blondjoe/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/header/Header";
+import Navbar from "./component/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
     </>
   );
 }
